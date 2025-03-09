60b24fb09fdd8539a72641e4caf903ff4f585e5b
Andy Jiang <72869031+jiangmuran@users.noreply.github.com>
2025-03-10 07:51:04 +0800

    Update Version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/jiangmuran/llm-apigateway'
 EMAIL = 'jiangmuran1224@gmail.com'
 AUTHOR = 'Andy Jiang'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
